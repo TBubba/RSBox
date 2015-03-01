28b1c787c2f170dce61c9602db3d2a8707c7f244
TBubba <xxbubbathedragonslayer007xx@gmail.com>
2015-03-02 00:12:55 +0100

    Initial Upload
    Current features:
    * Opens RuneScape in a Form
    * Removes "menu" (class) from html
    
    Next todo:
    * Remove all other html (everything but the client itself)
    * Allign webbrowser with form better (to remove waste of pixels and a
    over all better look)
    * Add (hidable) tool strip for accessing settings and such
    * Add (hidable) side-bar to where utility applications (calculator, max
    hit, highscores etc.) will be held
    
    (Sorry for bad english/grammar :>)

---
+++ RSBox/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace RSBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._bRemoveHTML = new System.Windows.Forms.Button();
+            this._browser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // _bRemoveHTML
+            // 
+            this._bRemoveHTML.Location = new System.Drawing.Point(4, 3);
+            this._bRemoveHTML.Name = "_bRemoveHTML";
+            this._bRemoveHTML.Size = new System.Drawing.Size(162, 23);
+            this._bRemoveHTML.TabIndex = 1;
+            this._bRemoveHTML.Text = "Remove other HTML content";
+            this._bRemoveHTML.UseVisualStyleBackColor = true;
+            this._bRemoveHTML.Click += new System.EventHandler(this._bRemoveHTML_Click);
+            // 
+            // _browser
+            // 
+            this._browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this._browser.Location = new System.Drawing.Point(1, 32);
+            this._browser.Margin = new System.Windows.Forms.Padding(0);
+            this._browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this._browser.Name = "_browser";
+            this._browser.ScriptErrorsSuppressed = true;
+            this._browser.ScrollBarsEnabled = false;
+            this._browser.Size = new System.Drawing.Size(760, 500);
+            this._browser.TabIndex = 0;
+            this._browser.WebBrowserShortcutsEnabled = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 533);
+            this.Controls.Add(this._browser);
+            this.Controls.Add(this._bRemoveHTML);
+            this.Name = "Form1";
+            this.Text = "RSBox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _bRemoveHTML;
+        private System.Windows.Forms.WebBrowser _browser;
+    }
+}
+
---
+++ RSBox/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using mshtml;
+
+namespace RSBox
+{
+    public partial class Form1 : Form
+    {
+        // Constructor(s)
+        public Form1()
+        {
+            //
+            InitializeComponent();
+
+            // Go to OldschoolRunescape
+            _browser.Url = new Uri("http://oldschool84.runescape.com/j1");
+        }
+
+        //
+        private List<HtmlElement> GetElementsByAttribute(HtmlDocument doc, string attribute, string value)
+        {
+            HtmlElementCollection collection = doc.All;
+            List<HtmlElement> list = new List<HtmlElement>();
+
+            for (int i = 0; i < collection.Count; i++)
+                if (collection[i].GetAttribute(attribute) == value)
+                    list.Add(collection[i]);
+
+            return list;
+        }
+        private void RemoveElement(HtmlElement element)
+        {
+            IHTMLElement tmpElement = (IHTMLElement)element.DomElement;
+            IHTMLDOMNode tmpNode = (IHTMLDOMNode)tmpElement;
+            tmpNode.parentNode.removeChild(tmpNode);
+        }
+
+        // Events
+        private void _bRemoveHTML_Click(object sender, EventArgs e)
+        {
+            List<HtmlElement> content;
+
+            // Remove Menu
+            content = GetElementsByAttribute(_browser.Document, "className", "menu");
+            if (content.Count >= 1)
+                RemoveElement(content[0]);
+
+            // Remove Ad (Need to find out how to get it)
+            //content = GetElementsByAttribute(_myBroswer.Document, "id", "AddOrSomeThing");
+            //if (content.Count >= 1)
+            //    RemoveElement(content[0]);
+        }
+    }
+}
